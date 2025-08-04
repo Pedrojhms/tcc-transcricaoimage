61f1a2aaaf164be43dc2da54e457a3837fd41b1b
pedrojhms <j.pedrohenrique2011@gmail.com>
2025-08-04 13:43:02 -0300

    Identificando tabela para inserção das métricas

--- src/main/java/tcc/transcricao/tcctranscricaoimage/model/PerformanceMetric.java
+++ src/main/java/tcc/transcricao/tcctranscricaoimage/model/PerformanceMetric.java
@@ -1,15 +1,17 @@
 package tcc.transcricao.tcctranscricaoimage.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 @Entity
+@Table(name = "performance_metric", schema = "public")
 public class PerformanceMetric {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
